0b8cf6f26c525d97c6b0463bc116dd51fe83f068
Indra Pramana <indra@ide.web.id>
2017-11-03 16:12:11 +0700

    update sore 03112017

--- application/models/MCetakQrCOde.php
+++ application/models/MCetakQrCOde.php
@@ -9,4 +9,48 @@ class MCetakQrCode extends CI_Model
 		$this->load->database();
 	}
 
+	public function listBPKBAll($sCari)
+	{
+		$xSQL = ("
+			SELECT fs_transaksi, fn_no_pjj, fs_nama_pemilik, fs_status,
+				fs_kode_lokasi, fs_kode_cabang, fs_kode_kendaraan, fs_jenis_kendaraan,
+				fn_tahun_kendaraan, fs_warna_kendaraan, fs_silinder_kendaraan,
+				fs_no_polisi, fs_no_rangka, fs_no_bpkb, fs_nama_bpkb, fd_tanggal_bpkb
+			FROM tx_bpkb
+		");
+
+		if (!empty($sCari)) {
+			$xSQL = $xSQL.("
+				WHERE fs_no_bpkb LIKE '%".trim($sCari)."%'
+			");
+		}
+
+		$sSQL = $this->db->query($xSQL);
+		return $sSQL;
+	}
+
+	public function listBPKB($sCari, $nStart, $nLimit)
+	{
+		$xSQL = ("
+			SELECT fs_transaksi, fn_no_pjj, fs_nama_pemilik, fs_status,
+				fs_kode_lokasi, fs_kode_cabang, fs_kode_kendaraan, fs_jenis_kendaraan,
+				fn_tahun_kendaraan, fs_warna_kendaraan, fs_silinder_kendaraan,
+				fs_no_polisi, fs_no_rangka, fs_no_bpkb, fs_nama_bpkb, fd_tanggal_bpkb
+			FROM tx_bpkb
+		");
+
+		if (!empty($sCari)) {
+			$xSQL = $xSQL.("
+				WHERE fs_no_bpkb LIKE '%".trim($sCari)."%'
+			");
+		}
+
+		$xSQL = $xSQL.("
+			ORDER BY fd_tanggal_buat DESC LIMIT ".$nStart.",".$nLimit."
+		");
+
+		$sSQL = $this->db->query($xSQL);
+		return $sSQL;
+	}
+
 }
